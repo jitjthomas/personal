2b56cf96aa0381cc74bd5f51052a1188adb625d2
jitjthomas <jit.j.thomas@gmail.com>
2016-05-27 11:41:46 +0530

    microservices training

---
+++ MigratingAMonolithApplicationToCloudNative/sessions/day_01/session_03/lab_09/initial/cities/src/main/java/org/example/cities/CitiesApplication.java
@@ -0,0 +1,16 @@
+package org.example.cities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
+
+@SpringBootApplication
+@EnableJpaRepositories
+@Import(RepositoryRestMvcConfiguration.class) // <---- And this
+public class CitiesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CitiesApplication.class, args);
+    }
+}
